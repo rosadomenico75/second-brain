fd47b90b9a6129b8454a1289ec41c78a007a0ef0
Domenico <rosadomenico75@gmail.com>
2026-08-30 12:58:35 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sb-shell-v1';
+const CACHE = 'sb-shell-v2';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
